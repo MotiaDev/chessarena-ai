1f5b7235378299b02eefd626f4d342d173f15a32
Rodrigo Morales <rodrigo@rodrigomorales.co>
2025-07-07 20:04:39 -0700

    fix: logic to download stockfish

--- api/steps/chess/evaluate-player-move.step.py
+++ api/steps/chess/evaluate-player-move.step.py
@@ -11,7 +11,7 @@ config = {
     "emits": ["player-move-score"],
     "flows": ["chess"],
     "input": None,
-    # NOTE: restore this once lambda deployments can support sourcing lambda code from s3 bucket
+    # NOTE: restore this once lambda deployments can be configured to support sourcing lambda code from s3 bucket
     # "includeFiles": ["./../../lib/stockfish"]
 }
 

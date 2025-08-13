5fb9cd7683eb303dc1b33153c4cf545dea1ba533
Sergio Marcelino <sergio@filho.org>
2025-08-13 11:04:09 -0300

    feat: adding gh icon to chess game page

--- app/src/components/motia-powered.tsx
+++ app/src/components/motia-powered.tsx
@@ -1,13 +1,22 @@
 import { cn } from '@/lib/utils'
 
-export const MotiaPowered: React.FC<{ size?: 'sm' | 'md' }> = ({ size = 'md' }) => {
+type Props = {
+  size?: 'sm' | 'md'
+  githubLogo?: boolean
+}
+
+export const MotiaPowered: React.FC<Props> = ({ size = 'md', githubLogo }) => {
   return (
     <a
       href="https://github.com/MotiaDev/chessarena-ai"
       target="_blank"
       className="flex flex-row md:flex-col gap-2 items-center justify-center"
     >
-      <img src="/icon-white.svg" alt="Motia Logo" className={cn('h-8', size === 'sm' && 'h-4')} />
+      <img
+        src={githubLogo ? '/github-white.svg' : '/icon-white.svg'}
+        alt="Motia Logo"
+        className={cn('h-8', size === 'sm' && 'h-6')}
+      />
       <p className="text-muted-foreground font-semibold text-sm">Powered by open-source</p>
     </a>
   )

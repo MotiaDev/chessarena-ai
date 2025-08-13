d8d235d970bf7cd6fdb6a8f3dd8e7c431f5d656e
Sergio Marcelino <sergio@filho.org>
2025-08-13 07:04:09 -0300

    fix: cron execution in cloud

--- api/steps/chess/09-purge-stuck-games.step.ts
+++ api/steps/chess/09-purge-stuck-games.step.ts
@@ -2,8 +2,9 @@ import { LiveAiGames } from '@chessarena/types/live-ai-games'
 import { CronConfig, Handlers } from 'motia'
 
 export const config: CronConfig = {
-  cron: '0 0 * * * *', // every hour
+  cron: '0 0 * * ? *', // every hour
   name: 'PurgeStuckGames',
+  description: 'Removes all games that have been stuck for more than 10 minutes',
   type: 'cron',
   emits: [],
   flows: ['chess'],

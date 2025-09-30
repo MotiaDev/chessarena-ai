5cc154f3cb61151918537ff85758ada3714e0f40
Glauber Castro <gfc714@gmail.com>
2025-09-30 09:37:39 -0300

    fix: landing page scrolling

--- app/src/pages/landing-page.tsx
+++ app/src/pages/landing-page.tsx
@@ -17,7 +17,7 @@ export const LandingPage = () => {
   usePageTitle('Powered by Motia')
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[1fr_minmax(min-content,200px)] w-screen md:h-dvh bg-image-landing">
+    <div className="grid grid-cols-1 md:grid-cols-[minmax(200px,1fr)_minmax(min-content,200px)] w-screen md:h-dvh bg-image-landing">
       <div className="hidden lg:flex lg:flex-col md:items-center md:justify-center p-4">
         <div className="w-full md:border-l-2 rounded-lg border border-white/5 backdrop-blur-lg">
           <div className="p-4 text-white text-center">
@@ -26,42 +26,40 @@ export const LandingPage = () => {
           <Leaderboard />
         </div>
       </div>
-      <div className="col-start-2 flex flex-col gap-4 items-center justify-between w-full h-dvh p-6 md:border-l-2 md:border-white/5 max-md:bg-black/60 md:backdrop-blur-lg overflow-y-auto">
-        <div className="flex flex-col flex-1 gap-4 items-center justify-between w-full h-full">
-          <MotiaPowered />
-          <div className="flex flex-col gap-2 items-center justify-center">
-            <img src="/horse.png" alt="Chessarena.ai" className="h-[160px] w-auto" />
-            <h1 className="text-6xl font-title text-white">Chessarena.ai</h1>
-            <p className="font-medium text-center text-muted-foreground">Welcome to Chessarena.ai powered by Motia!</p>
-            <p className="font-medium text-center text-muted-foreground">
-              Chessarena.ai was created to show how leading models compete against each other in chess games.{' '}
-              <a href="/about" className="text-white underline" onClick={goToAbout}>
-                Click here to learn more.
-              </a>
-            </p>
+      <div className="flex flex-col w-full p-6 gap-4 items-center justify-between col-start-2 md:border-l-2 md:border-white/5 max-md:bg-black/60 md:backdrop-blur-lg overflow-y-auto">
+        <MotiaPowered />
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <img src="/horse.png" alt="Chessarena.ai" className="h-[160px] w-auto" />
+          <h1 className="text-6xl font-title text-white">Chessarena.ai</h1>
+          <p className="font-medium text-center text-muted-foreground">Welcome to Chessarena.ai powered by Motia!</p>
+          <p className="font-medium text-center text-muted-foreground">
+            Chessarena.ai was created to show how leading models compete against each other in chess games.{' '}
+            <a href="/about" className="text-white underline" onClick={goToAbout}>
+              Click here to learn more.
+            </a>
+          </p>
+        </div>
+
+        <AuthContainer />
+
+        <div className="flex flex-col gap-6 items-center justify-center w-full">
+          <CreateGameButton onClick={() => navigate('/new')}>Create Game</CreateGameButton>
+          <div className="flex flex-row gap-2 items-center justify-center w-full">
+            <BaseButton className="w-full flex-1" onClick={() => navigate('/live-matches')}>
+              View live matches
+            </BaseButton>
+            <BaseButton onClick={() => navigate('/leaderboard')}>
+              <Trophy /> Leaderboard
+            </BaseButton>
           </div>
 
-          <AuthContainer />
-
-          <div className="flex flex-col gap-6 items-center justify-center w-full">
-            <CreateGameButton onClick={() => navigate('/new')}>Create Game</CreateGameButton>
-            <div className="flex flex-row gap-2 items-center justify-center w-full">
-              <BaseButton className="w-full flex-1" onClick={() => navigate('/live-matches')}>
-                View live matches
-              </BaseButton>
-              <BaseButton onClick={() => navigate('/leaderboard')}>
-                <Trophy /> Leaderboard
-              </BaseButton>
-            </div>
-
-            <p className="font-medium text-sm text-center text-muted-foreground">
-              This project is open-source, click{' '}
-              <a href="/about" className="text-white underline" onClick={goToAbout}>
-                here
-              </a>{' '}
-              to read more about the project.
-            </p>
-          </div>
+          <p className="font-medium text-sm text-center text-muted-foreground">
+            This project is open-source, click{' '}
+            <a href="/about" className="text-white underline" onClick={goToAbout}>
+              here
+            </a>{' '}
+            to read more about the project.
+          </p>
         </div>
       </div>
     </div>

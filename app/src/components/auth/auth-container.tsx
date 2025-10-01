46f94cb5eb733c1a9fb889df9ab360f7fcac39d1
Glauber Castro <gfc714@gmail.com>
2025-09-30 21:32:14 -0300

    fix: inconsistent auth container height

--- app/src/components/auth/auth-container.tsx
+++ app/src/components/auth/auth-container.tsx
@@ -5,13 +5,23 @@ import { Loader2, LogIn } from 'lucide-react'
 import { useNavigate } from 'react-router'
 
 export const AuthContainer = () => {
-  const { user, isLoading, logout } = useAuth()
+  const {
+    // user,
+    isLoading,
+    logout,
+  } = useAuth()
   const navigate = useNavigate()
+  // const user = undefined
+  const user = {
+    name: 'John Doe',
+    profilePic: 'https://via.placeholder.com/150',
+    email: 'john.doe@example.com',
+  }
 
   if (user) {
     return (
-      <div className="flex items-center justify-between w-[400px] max-w-full rounded-xl backdrop-blur-lg bg-white/10 px-4 py-4 shadow-lg">
-        <div className="flex items-center space-x-3">
+      <div className="flex items-center justify-between w-full rounded-xl backdrop-blur-lg bg-white/10 px-4 py-4 shadow-lg">
+        <div className="flex items-center gap-3">
           <div className="w-10 h-10 rounded-full bg-black/50 flex items-center justify-center">
             <ChatBubbleAvatar color={'white'} fallback={user.name?.slice(0, 2).toUpperCase()} src={user.profilePic} />
           </div>
@@ -21,28 +31,27 @@ export const AuthContainer = () => {
           </div>
         </div>
 
-        <div className="flex items-center space-x-2">
-          <Button variant="outline" onClick={logout}>
-            Sign Out
-          </Button>
-        </div>
+        <Button variant="outline" onClick={logout}>
+          Sign Out
+        </Button>
       </div>
     )
   }
 
   return (
-    <div className="flex items-center justify-between w-[400px] rounded-xl backdrop-blur-lg bg-white/10 px-4 py-2 shadow-lg">
-      <div className="flex items-center space-x-3">
-        <div className="w-10 h-10 rounded-full bg-white flex items-center justify-center">
+    <div className="flex items-center justify-between w-full gap-2 rounded-xl backdrop-blur-lg bg-white/10 px-4 py-4 shadow-lg">
+      <div className="flex items-center gap-3">
+        <div className="p-3 rounded-full bg-white">
           <LogIn className="size-4 text-black" />
         </div>
         <div>
           <div className="text-white font-medium text-lg">Sign In</div>
-          <div className="text-gray-400 text-sm">You need an account to play</div>
+          <div className="block sm:hidden text-gray-400 text-sm">Sign In to play</div>
+          <div className="hidden sm:block text-gray-400 text-sm">You need an account to play</div>
         </div>
       </div>
 
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center gap-2">
         {isLoading ? (
           <Button variant="outline">
             <Loader2 className="size-4 animate-spin" />

9fc96ecea2a1d483ea09b4e799127228936a9b33
Glauber Castro <gfc714@gmail.com>
2025-10-03 10:55:43 -0300

    fix: background and border colors

--- app/src/components/ui/selector.tsx
+++ app/src/components/ui/selector.tsx
@@ -13,8 +13,8 @@ export const Selector: React.FC<Props> = ({ isSelected, className, children, onC
       onClick={onClick}
       className={cn(
         'flex flex-row gap-2 items-center justify-center rounded-md border-[2px] p-6 font-semibold text-md cursor-pointer',
-        !isSelected && 'border-transparent bg-white/3 hover:border-white/30',
-        isSelected && 'border-white/40 bg-white/5',
+        !isSelected && ' bg-transparent hover:bg-white/5',
+        isSelected && 'bg-white/10 border-white/40',
         !onClick && 'cursor-default border-white/5',
         className,
       )}

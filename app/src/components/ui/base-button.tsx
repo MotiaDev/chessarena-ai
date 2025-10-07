3b309805418e6aa5a20c48e05600f77f1feb8e9e
Glauber Castro <gfc714@gmail.com>
2025-10-07 15:19:57 -0300

    feat: improve base button styles

--- app/src/components/ui/base-button.tsx
+++ app/src/components/ui/base-button.tsx
@@ -1,7 +1,6 @@
 import { cn } from '@/lib/utils'
 import { Loader2 } from 'lucide-react'
 import * as React from 'react'
-import './base-button.css'
 
 type Props = React.ComponentProps<'button'> & {
   isLoading?: boolean
@@ -9,7 +8,13 @@ type Props = React.ComponentProps<'button'> & {
 
 export const BaseButton: React.FC<Props> = ({ className, children, isLoading, ...props }) => {
   return (
-    <button className={cn('chess-arena-button', className)} {...props}>
+    <button
+      className={cn(
+        'base-button flex items-center justify-center h-16 py-2 px-4 gap-2 rounded-md select-none relative font-extrabold border border-transparent cursor-pointer whitespace-nowrap',
+        className,
+      )}
+      {...props}
+    >
       {isLoading ? <Loader2 className="size-4 animate-spin" /> : children}
     </button>
   )

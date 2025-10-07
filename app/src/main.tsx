a6fd89f7b21a8dea9319d39ca8a4f78a10642701
Glauber Castro <gfc714@gmail.com>
2025-10-07 16:47:56 -0300

    feat: move chess styles to index

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,11 +4,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import '@lichess-org/chessground/assets/chessground.base.css'
-import '@lichess-org/chessground/assets/chessground.brown.css'
-import '@lichess-org/chessground/assets/chessground.cburnett.css'
-import './chess.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

d110893fb23157c42ec8659ce88f4e7e7f607db4
Glauber Castro <gfc714@gmail.com>
2025-10-16 11:49:13 -0300

    feat: improve styles in scoreboard

--- app/src/components/chess/scoreboard/matchup.tsx
+++ app/src/components/chess/scoreboard/matchup.tsx
@@ -13,10 +13,10 @@ export const Matchup: React.FC<MatchupProps> = ({ white, black }) => (
     {/* White Side */}
     <div className="flex items-start gap-2 justify-start bg-white text-black px-4 py-3 w-48 relative flex-1">
       {white.ai && <AiIcon ai={white.ai} color="black" />}
-      <div className="flex flex-col">
+      <div className="flex flex-col self-stretch">
         {white.ai && <span className="font-bold capitalize">{white.ai ?? 'White'}</span>}
-        {white.model && <span className="text-xs text-gray-600">{white.model}</span>}
-        <div className="flex gap-2">
+        {white.model && <span className="grow text-xs text-gray-600">{white.model}</span>}
+        <div className="flex gap-2 mt-2">
           <ChessIcon color="white" size={20} transparent />
           <span className="font-semibold">White</span>
         </div>
@@ -24,12 +24,12 @@ export const Matchup: React.FC<MatchupProps> = ({ white, black }) => (
     </div>
 
     <div className="flex items-start gap-2 justify-end bg-black text-white px-4 py-3 w-48 relative flex-1">
-      <div className="flex flex-col">
+      <div className="flex flex-col self-stretch items-end">
         {black.ai && <span className="font-bold text-right capitalize">{black.ai ?? 'Black'}</span>}
-        {black.model && <span className="text-xs text-gray-400 text-right">{black.model}</span>}
-        <div className="flex gap-2 text-right">
-          <ChessIcon color="black" size={20} transparent />
+        {black.model && <span className="grow text-xs text-gray-400 text-right">{black.model}</span>}
+        <div className="flex gap-2 mt-2 text-right">
           <span className="font-semibold">Black</span>
+          <ChessIcon color="black" size={20} transparent />
         </div>
       </div>
       {black.ai && <AiIcon ai={black.ai} color="white" />}

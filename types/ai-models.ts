ec8dd86f5fd7e5014c1285c2bf2aba2e4c2a2b76
Glauber Castro <gfc714@gmail.com>
2025-10-16 16:30:43 -0300

    feat: update schema to use describe function

--- types/ai-models.ts
+++ types/ai-models.ts
@@ -14,22 +14,21 @@ export const AiProviderDefaultModelSchema = z.object(
 )
 
 export const AiPlayerPromptSchema = z.object({
-  thought: z.string({
-    description:
-      'The thought process of the move, make it look like you were just thinking for yourself, this is not an explanation to someone else',
-  }),
-  move: z.object(
-    {
-      from: z.string({ description: 'The square to move from, example: e2, Make sure to move from a valid square' }),
-      to: z.string({ description: 'The square to move to, example: e4. Make sure to move to a valid square' }),
+  thought: z
+    .string()
+    .describe(
+      'A short and concise thought process of the move. Make it look like you were just thinking for yourself, this is not an explanation to someone else. Max 100 characters.',
+    ),
+  move: z
+    .object({
+      from: z.string().describe('The square to move from, example: e2, Make sure to move from a valid square'),
+      to: z.string().describe('The square to move to, example: e4. Make sure to move to a valid square'),
       promote: z
-        .enum(['queen', 'rook', 'bishop', 'knight'], {
-          description: "The promotion piece, if any. Don't include if no promotion",
-        })
-        .optional(),
-    },
-    { description: 'Your move, make sure to move from a valid square and to a valid square' },
-  ),
+        .enum(['queen', 'rook', 'bishop', 'knight'])
+        .optional()
+        .describe("The promotion piece, if any. Don't include if no promotion"),
+    })
+    .describe('Your move, make sure to move from a valid square and to a valid square'),
 })
 
 export type AiModels = z.infer<ReturnType<typeof AiModelsSchema>>
